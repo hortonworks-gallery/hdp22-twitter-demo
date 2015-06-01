ca193ae56d7e831c094b6ed85c5f2eb3f8886a23
gbraccialli <gbraccialli@gmail.com>
2015-06-01 18:49:21 -0300

    - FIX: date format (it was using hh:mm:ss, corret is HH:mm:ss) - removed hashtag doc, now using hashtag count based on tweet document - new documment id for tweet document, previous code, allowed only one tweet by user (I thing there as a type on tweet - missing quotes) - added tag as multi-value field on tweet document - added polarity information (based on this tutorial http://hortonworks.com/hadoop-tutorial/how-to-refine-and-visualize-sentiment-data/)   count words polarity on phoenix table

--- stormtwitter-mvn/src/main/java/hellostorm/SolrBolt.java
+++ stormtwitter-mvn/src/main/java/hellostorm/SolrBolt.java
@@ -3,6 +3,8 @@ package hellostorm;
 import java.io.File;
 import java.io.FileWriter;
 import java.util.Map;
+import java.util.Arrays;
+import java.util.Properties;
 
 import backtype.storm.topology.BasicOutputCollector;
 import backtype.storm.task.TopologyContext;
@@ -11,22 +13,39 @@ import backtype.storm.topology.OutputFieldsDeclarer;
 import backtype.storm.tuple.Tuple;
 import backtype.storm.task.OutputCollector;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.Driver;
+import java.sql.SQLException;
+
+import org.apache.phoenix.jdbc.PhoenixDriver;
 import org.apache.solr.client.solrj.SolrServer;
 import org.apache.solr.client.solrj.SolrServerException;
 import org.apache.solr.client.solrj.impl.HttpSolrServer;
 import org.apache.solr.common.SolrInputDocument;
 
 import java.util.Date;
+import java.text.BreakIterator;
 import java.text.SimpleDateFormat;
 
 public class SolrBolt extends BaseBasicBolt {
 
 	private SolrServer server = null;
 
+	private static Driver phoenixDriver = new PhoenixDriver();
+	Connection conn = null;
+
 	@Override
 	public void prepare(Map stormConf, TopologyContext context) {
 	
 		server = new HttpSolrServer("http://sandbox.hortonworks.com:8983/solr/tweets");
+		try{
+			conn = phoenixDriver.connect("jdbc:phoenix:sandbox.hortonworks.com:2181:/hbase-unsecure",new Properties());
+		}catch (SQLException e){
+			System.err.println("error when connection to phoenix" + e.toString());
+		}
+
 	}
 
 	@Override
@@ -46,36 +65,25 @@ public class SolrBolt extends BaseBasicBolt {
 		Date  d=new Date(created);
 		//SimpleDateFormat formatter=new SimpleDateFormat("yyyy-MM-dd'T'hh:mm:ss'Z+9HOUR'");
 		//SimpleDateFormat formatter=new SimpleDateFormat("yyyy-MM-dd'T'hh:mm:ss'Z-4HOUR'");
-		SimpleDateFormat formatter=new SimpleDateFormat("yyyy-MM-dd'T'hh:mm:ss'Z'");
+		SimpleDateFormat formatter=new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss'Z'");
 		created = formatter.format(d);
-
-		//create a hashtag doc
-		SolrInputDocument hashtagDoc = new SolrInputDocument();
-       		 hashtagDoc.addField("id", userId + ":" + "hashtag");
-        	hashtagDoc.addField("doctype_s", "hashtag");
-        	hashtagDoc.addField("createdAt_dt", created);
-        	hashtagDoc.addField("tag_s", hashtag);		
-
-
-		try{
-			server.add(hashtagDoc);	
-			System.out.println("SolrBolt: successfully added hastag to Solr server");
-				
-		} catch (Exception e) {
-			e.printStackTrace();			
-		}		
 
 		//create a tweet doc
 		SolrInputDocument doc = new SolrInputDocument();
 
-	        doc.addField("id", userId + ":" + "tweet");
+	        doc.addField("id", userId + ":" + tweet);
         	doc.addField("doctype_s", "tweet");
-       		 doc.addField("createdAt_dt", created);
+       		doc.addField("createdAt_dt", created);
         	doc.addField("text_t", tweet);
+            doc.addField("tag_ss", Arrays.asList(hashtag.split(" ")));
         	doc.addField("language_s", language);
         	doc.addField("screenName_s", displayname);
         	doc.addField("source_s", "");
         	doc.addField("location_s", "");
+        	doc.addField("polarity_s", getPolarity(tweet));
+        	doc.addField("polarityCount_i", 1);
+
+        	
 		
 		try{
 			server.add(doc);
@@ -88,7 +96,57 @@ public class SolrBolt extends BaseBasicBolt {
 
 	}
 
+	private String getPolarity(String tweet){
+
+		String polarity = "neutral";
+		tweet = tweet.toLowerCase();
+		BreakIterator wi = BreakIterator.getWordInstance();
+		StringBuffer strBuffer = new StringBuffer();
+		wi.setText(tweet);
+		int widx = 0;
+		while(wi.next() != BreakIterator.DONE) {
+			String word = tweet.substring(widx, wi.current());
+			widx = wi.current();
+			if(Character.isLetterOrDigit(word.charAt(0))) {
+				word = word.replaceAll("'", "\\\\'");
+				strBuffer.append("word = '" +  word + "' OR ");
+			}
+		}
+
+
+		try {
+			String query = "select case when positive > negative then 'positive' when negative > positive then 'negative' else 'neutral' end as polarity from (select coalesce(sum(case when polarity='positive' then 1 end),0) as positive, coalesce(sum(case when polarity='negative' then 1 end),0) as negative from dictionary where (" + strBuffer.substring(0, strBuffer.length()-4) + ")) t1";
+			System.out.println(query);
+			ResultSet rst = conn.createStatement().executeQuery(query);
+
+			while (rst.next()) {
+				polarity = rst.getString(1);
+			}
+
+		}
+		catch(Exception e){
+			e.printStackTrace();
+			//ERROR SELECTION POLARITY IGNOR
+			System.err.println("Error selection polarity, erro will be ignored and polarity will be considered 'neutral'. error: " + e.toString());
+		}
+
+		return polarity;
+	}
+
+	
     @Override
+	public void cleanup() {
+		// TODO Auto-generated method stub
+		super.cleanup();
+		try{
+			conn.close();
+		}catch (SQLException e){
+			System.err.println("Error when closing phoenix connection " +  e.toString());
+		}
+		conn = null;
+	}
+
+	@Override
     public void declareOutputFields(OutputFieldsDeclarer declarer) {
         // no-op
     }
