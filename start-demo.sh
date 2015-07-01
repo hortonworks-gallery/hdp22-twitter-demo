57132a635899f9ff4c46fff4c8dcf898126b0a5a
Ali Bajwa <abajwa@hortonworks.com>
2015-07-01 11:41:57 +0100

    only purge local mvn on first compile

--- start-demo.sh
+++ start-demo.sh
@@ -21,5 +21,12 @@ startWait KAFKA
 
 /root/hdp22-twitter-demo/setup-scripts/restart_solr_banana.sh
 cd ~/hdp22-twitter-demo/stormtwitter-mvn
-mvn dependency:purge-local-repository
+if [ ! -f target/storm-streaming-1.0-SNAPSHOT.jar ]
+then
+	echo "First compile detected, running mvn purge local"
+	mvn dependency:purge-local-repository
+else
+	echo "Uber jar found, running regular mvn compile"	
+fi	
 ./runtopology.sh
+
