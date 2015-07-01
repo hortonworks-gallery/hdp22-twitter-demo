a036eb3a2537de4bd1167f73b4bd76f2ecff3522
Ali Bajwa <abajwa@hortonworks.com>
2015-07-01 22:53:20 +0100

    fixed demo reset script

--- setup-scripts/restart_solr_banana.sh
+++ setup-scripts/restart_solr_banana.sh
@@ -11,7 +11,9 @@ cp -f /opt/banana/latest/jetty-contexts/banana-context.xml /opt/solr/latest/hdp/
 echo "Resetting Solr...."
 ps -ef | grep sol[r] | awk '{print $2}' | sudo xargs kill
 rm  -f /opt/solr/latest/hdp/solr/tweets/core.properties 
-hadoop fs -rm -r /user/solr/tweets
+sudo -u hdfs hadoop fs -rm -r /user/solr/tweets
+sudo -u hdfs hadoop fs -mkdir /user/solr/tweets
+sudo -u hdfs hadoop fs -chmod -R 777 /user/solr
 nohup java -jar /opt/solr/latest/hdp/start.jar -Djetty.home=/opt/solr/latest/hdp -Dsolr.solr.home=/opt/solr/latest/hdp/solr &> /root/hdp22-twitter-demo/logs/solr.out &
 sleep 30
 curl "http://localhost:8983/solr/admin/cores?action=CREATE&name=tweets&instanceDir=/opt/solr/latest/hdp/solr/tweets"
