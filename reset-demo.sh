4bee5218177510ba9108cd85188e8a39211408cb
Ali Bajwa <abajwa@hortonworks.com>
2015-07-01 22:10:30 +0100

    on demo reset, recreate HDFS dirs with open permissions

--- reset-demo.sh
+++ reset-demo.sh
@@ -7,9 +7,9 @@ rm -f /tmp/Tweets.xls
 echo "Re-creating /tweets/staging HDFS dir"
 sudo -u hdfs hadoop fs -rm -R /tweets
 sudo -u hdfs hadoop fs -mkdir /tweets
-sudo -u hdfs hadoop fs -chmod +rwx /tweets
+sudo -u hdfs hadoop fs -chmod 777 /tweets
 sudo -u hdfs hadoop fs -mkdir /tweets/staging
-sudo -u hdfs hadoop fs -chmod +rwx /tweets/staging
+sudo -u hdfs hadoop fs -chmod 777 /tweets/staging
 
 echo "Re-creating tweets hive tables"
 hive -f /root/hdp22-twitter-demo/stormtwitter-mvn/twitter.sql
