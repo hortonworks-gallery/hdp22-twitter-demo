573e7bbbef9746ceff694239e7605ee39ac90477
gbraccialli <gbraccialli@gmail.com>
2015-12-22 20:54:08 -0200

    more sleep.

--- setup-scripts/install_solr_2.3.2.sh
+++ setup-scripts/install_solr_2.3.2.sh
@@ -8,7 +8,7 @@ sudo -u hdfs hadoop fs -chown solr /user/solr
 chown -R solr:solr /opt/lucidworks-hdpsearch/solr
 sudo -u solr /opt/lucidworks-hdpsearch/solr/bin/solr start -c -z localhost:2181
 
-sleep 5
+sleep 10
 
 sudo -u solr /opt/lucidworks-hdpsearch/solr/bin/solr create -c tweets \
    -d data_driven_schema_configs \
