75a828c95396eb16db50b00c30e268b0a4e0c852
gbraccialli <gbraccialli@gmail.com>
2015-12-22 20:55:29 -0200

    pom and setup for 2.3.2

--- start-demo-230.sh
+++ start-demo-230.sh
@@ -25,8 +25,7 @@ startWait HBASE
 echo '*** Starting kafka....'
 startWait KAFKA
 
-sudo -u solr /opt/lucidworks-hdpsearch/solr/bin/solr start -c -z localhost:2181
-
+/root/hdp22-twitter-demo/setup-scripts/restart_solr_banana.sh
 cd ~/hdp22-twitter-demo/stormtwitter-mvn
 if [ ! -f target/storm-streaming-1.0-SNAPSHOT.jar ]
 then
